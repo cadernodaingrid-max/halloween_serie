ca776a5accd8266474ab5f479704ab3546c1a5c9
Ingrid <caderno.da.ingrid@gmail.com>
2025-11-05 10:29:13 -0400

    Update script_boxe.js

--- boxe/script_boxe.js
+++ boxe/script_boxe.js
@@ -4,6 +4,7 @@ let elementoBotao;
 
 // Função para exibir o exercício
 function exibirExercicio(indice) {
+
     if (!elementoExercicio || !window.treino || window.treino.length === 0) return; 
     
 
@@ -19,13 +20,13 @@ function proximoExercicio() {
         indiceAtual++;
         exibirExercicio(indiceAtual);
         
-
+        // Altera o texto do botão na primeira vez
         if (elementoBotao.textContent.trim().includes('DONE') || elementoBotao.textContent.trim().includes('INICIAR')) {
             elementoBotao.textContent = 'PRÓXIMO';
         }
 
     } else if (indiceAtual === window.treino.length - 1) {
-
+        // Ao chegar no último item
         elementoExercicio.innerHTML = window.treino[indiceAtual];
         elementoBotao.style.display = 'none';
     }
@@ -35,8 +36,8 @@ function proximoExercicio() {
 document.addEventListener('DOMContentLoaded', (event) => {
     elementoExercicio = document.getElementById('exercicio-atual');
     elementoBotao = document.getElementById('proximo-btn');
-
-  
+    
+    // Checagem de erro para a lista de exercícios
     if (!window.treino || !Array.isArray(window.treino) || window.treino.length === 0) {
         console.error("Erro: Array 'treino' não definido ou vazio no HTML.");
         document.body.innerHTML = '<h1 style="color:red; font-size: 5vw;">ERRO: O treino não foi carregado. Verifique a lista no HTML.</h1>';
